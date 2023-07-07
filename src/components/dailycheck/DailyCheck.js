34614ddd5487ab5d8a2b713985d6bb4ef76abd25
Commeci <cjtkemfldj@naver.com>
2023-07-07 22:06:53 +0900

    일일분석 페이지 중복 div 제거

--- src/components/dailycheck/DailyCheck.js
+++ src/components/dailycheck/DailyCheck.js
@@ -7,36 +7,36 @@ import axios from "axios";
 const API_URL = "http://43.201.73.166:8080";
 
 const DailyCheck = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [progressData, setProgressData] = useState("");
+    const [progressData, setProgressData] = useState("");
 
-  const moveHomePage = () => {
-    navigate("/");
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let token = localStorage.getItem("accessToken");
-        const res = await axios.get(`${API_URL}/api/analysis/daily`, {
-          headers: {
-            "Content-Type": "application/json",
-            // Authorization: `Bearer ${localStorage.getItem(token)}`,
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        const intakeData = res.data;
-        setProgressData(intakeData);
-        console.log(intakeData);
-      } catch (error) {
-        console.error("intakeData bring Failed!");
-      }
+    const moveHomePage = () => {
+        navigate("/");
     };
 
-    fetchData();
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let token = localStorage.getItem("accessToken");
+                const res = await axios.get(`${API_URL}/api/analysis/daily`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        // Authorization: `Bearer ${localStorage.getItem(token)}`,
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                const intakeData = res.data;
+                setProgressData(intakeData);
+                console.log(intakeData);
+            } catch (error) {
+                console.error("intakeData bring Failed!");
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div className="daily_page">
@@ -110,21 +110,7 @@ const DailyCheck = () => {
                 </button>
             </div>
         </div>
-      </div>
-      <div className="classify_graph">
-        <p className="description">
-          당신의 일일 적정 섭취량은 칼로리 2100kacl, 탄수화물 1300kacl, 단백질 50g, 지방 70g입니다.
-          현재 당신의 섭취량은 칼로리, 탄수화물, 단백질, 지방 모두 일일 적정량에 미달 상태입니다.
-          일일 적정량을 더 섭취하여 권장량을 맞춰주세요.
-        </p>
-      </div>
-      <div className="button_group">
-        <button className="home_btn" onClick={moveHomePage}>
-          홈으로
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DailyCheck;
