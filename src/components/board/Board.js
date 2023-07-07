8937c8789dae4b8b6d4a54c554bb8224f852fd81
Moyujin <scab12341@gmail.com>
2023-07-07 21:22:00 +0900

    게시판 기능 추가

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,79 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/board/Board.css";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import CreateIcon from "@mui/icons-material/Create";
+import axios from "axios";
+import { changeLikeStatus, getBoardList } from "../controller/BoardController";
+import { useNavigate } from "react-router-dom";
+
 const Board = () => {
+  const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => setBoardList(await getBoardList()))();
+  }, []);
+
+  const onCheckIcon = async (id) => {
+    const loginToken = localStorage.getItem("accessToken");
+    const data = {
+      boardId: id,
+      likeClicked: true,
+    };
+
+    const res = await changeLikeStatus(data);
+  };
+
   return (
     <div className="boardContainer">
-      <div className="post">
-        <div className="header">
-          <img className="userImg" src="/logo512.png" alt="error" />
-          <p className="userName">밥 먹는 꼼시</p>
-        </div>
-        <div className="content">
-          <img className="postImg" src="/logo512.png" alt="error" />
-          <FavoriteBorderIcon />
-          <p>블라라랄라ㅏ</p>
-          <div className="tagBox">
-            <p className="tag">라볶이</p>
-            <p className="tag">치킨</p>
-            <p className="tag">샐러드</p>
-            <p className="tag">샐러드</p>
+      {boardList.map((boardItem, idx) => {
+        return (
+          <div className="post">
+            <div className="header">
+              <img className="userImg" src={boardItem.profileImg} alt="error" />
+              <p className="userName">{boardItem.author}</p>
+            </div>
+            <div className="content">
+              <img className="postImg" src={boardItem.images[0]} alt="error" />
+              <p onClick={() => onCheckIcon(boardItem.id)}>
+                {boardItem.isLike === true ? <h1>클릭됨</h1> : null}
+                <FavoriteBorderIcon />
+              </p>
+              <p>{boardItem.description}</p>
+            </div>
+            <div className="tagBox">
+              {boardItem.tags.map((tag) => {
+                return <p>tag</p>;
+              })}
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="post">
-        <div className="header">
-          <img className="userImg" src="/logo512.png" alt="error" />
-          <p className="userName">밥 먹는 꼼시</p>
-        </div>
-        <div className="content">
-          <img className="postImg" src="/logo512.png" alt="error" />
-          <FavoriteBorderIcon />
-          <p>블라라랄라ㅏ</p>
-          <div className="tagBox">
-            <p className="tag">라볶이</p>
-            <p className="tag">치킨</p>
-            <p className="tag">샐러드</p>
-            <p className="tag">샐러드</p>
-          </div>
-        </div>
-      </div>
-      <div className="post">
-        <div className="header">
-          <img className="userImg" src="/logo512.png" alt="error" />
-          <p className="userName">밥 먹는 꼼시</p>
-        </div>
-        <div className="content">
-          <img className="postImg" src="/logo512.png" alt="error" />
-          <FavoriteBorderIcon />
-          <p>블라라랄라ㅏ</p>
-          <div className="tagBox">
-            <p className="tag">라볶이</p>
-            <p className="tag">치킨</p>
-            <p className="tag">샐러드</p>
-            <p className="tag">샐러드</p>
-          </div>
-        </div>
-      </div>
-      <div className="post">
-        <div className="header">
-          <img className="userImg" src="/logo512.png" alt="error" />
-          <p className="userName">밥 먹는 꼼시</p>
-        </div>
-        <div className="content">
-          <img className="postImg" src="/logo512.png" alt="error" />
-          <FavoriteBorderIcon />
-          <p>블라라랄라ㅏ</p>
-          <div className="tagBox">
-            <p className="tag">라볶이</p>
-            <p className="tag">치킨</p>
-            <p className="tag">샐러드</p>
-            <p className="tag">샐러드</p>
-          </div>
-        </div>
-      </div>
-      <CreateIcon />
+        );
+      })}
+
+      <button className="writeBtn" onClick={() => navigate("/write")}>
+        <CreateIcon />
+      </button>
     </div>
   );
 };
