732c18e1b0a18126279c2207cdbd8a6b70b3eab8
Commeci <cjtkemfldj@naver.com>
2023-07-08 01:34:21 +0900

    dailypage text modify

--- src/components/dailycheck/DailyCheck.js
+++ src/components/dailycheck/DailyCheck.js
@@ -3,111 +3,174 @@ import { useNavigate } from "react-router-dom";
 import ProgressBar from "@ramonak/react-progress-bar";
 import axios from "axios";
 import "../../styles/dailyCheck/DailyCheck.css";
+import textData from "./DailyAnalysisText";
 
 const API_URL = "http://43.201.73.166:8080";
 
 const DailyCheck = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [progressData, setProgressData] = useState("");
+    const [progressData, setProgressData] = useState("");
 
-  const moveHomePage = () => {
-    navigate("/");
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let token = localStorage.getItem("accessToken");
-        const res = await axios.get(`${API_URL}/api/analysis/daily`, {
-          headers: {
-            "Content-Type": "application/json",
-            // Authorization: `Bearer ${localStorage.getItem(token)}`,
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        const intakeData = res.data;
-        setProgressData(intakeData);
-        console.log(intakeData);
-      } catch (error) {
-        console.error("intakeData bring Failed!");
-      }
+    const moveHomePage = () => {
+        navigate("/");
     };
 
-    fetchData();
-  }, []);
+    const [analysisText, setAnalysisText] = useState({
+        calorie: "",
+        salt: "",
+        protein: "",
+        fat: "",
+    });
 
-  return (
-    <div className="daily_page">
-      <div className="title_group">
-        <p className="title">Food Fit</p>
-        <p className="small_title">일일 분석</p>
-      </div>
-      <div className="daily_graph">
-        <div className="bar">
-          <p className="element">칼로리&nbsp;&nbsp;&nbsp;&nbsp;</p>
-          <ProgressBar
-            className="progress"
-            completed={progressData.totalCaloriePerDay}
-            maxCompleted={progressData.recommendedCaloriePerDay}
-            customLabel={`${progressData.totalCaloriePerDay}`}
-            labelClassName="label"
-            bgColor="#B2F7EF"
-            barContainerClassName="container1"
-          />
+    useEffect(() => {
+        const text = () => {
+            const nutrients = ["calorie", "protein", "fat", "salt"];
+
+            for (let i = 0; i < nutrients.length; i++) {
+                const nutrient = nutrients[i];
+                const recommendedAmount =
+                    progressData[
+                        `recommended${
+                            nutrient.charAt(0).toUpperCase() + nutrient.slice(1)
+                        }PerDay`
+                    ];
+                const totalAmount =
+                    progressData[
+                        `total${
+                            nutrient.charAt(0).toUpperCase() + nutrient.slice(1)
+                        }PerDay`
+                    ];
+                const textGoodKey = `${nutrient}GoodText`;
+                const textOverKey = `${nutrient}OverText`;
+                const textLackKey = `${nutrient}LackText`;
+
+                if (
+                    Math.abs(recommendedAmount - totalAmount) <=
+                    recommendedAmount * 0.1
+                ) {
+                    setAnalysisText((prevAnalysisText) => ({
+                        ...prevAnalysisText,
+                        [nutrient]: textData[textGoodKey],
+                    }));
+                } else if (
+                    recommendedAmount - totalAmount <
+                    -recommendedAmount * 0.1
+                ) {
+                    setAnalysisText((prevAnalysisText) => ({
+                        ...prevAnalysisText,
+                        [nutrient]: textData[textOverKey],
+                    }));
+                } else {
+                    setAnalysisText((prevAnalysisText) => ({
+                        ...prevAnalysisText,
+                        [nutrient]: textData[textLackKey],
+                    }));
+                }
+            }
+        };
+
+        const fetchData = async () => {
+            try {
+                let token = localStorage.getItem("accessToken");
+                const res = await axios.get(`${API_URL}/api/analysis/daily`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        // Authorization: `Bearer ${localStorage.getItem(token)}`,
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                const intakeData = res.data;
+                setProgressData(intakeData);
+                console.log(intakeData);
+                text();
+            } catch (error) {
+                console.error("intakeData bring Failed!");
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="daily_page">
+            <div className="title_group">
+                <p className="title">Food Fit</p>
+                <p className="small_title">일일 분석</p>
+            </div>
+            <div className="daily_graph">
+                <div className="bar">
+                    <p className="element">칼로리&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                    <ProgressBar
+                        className="progress"
+                        completed={progressData.totalCaloriePerDay}
+                        maxCompleted={progressData.recommendedCaloriePerDay}
+                        customLabel={`${progressData.totalCaloriePerDay}`}
+                        labelClassName="label"
+                        bgColor="#B2F7EF"
+                        barContainerClassName="container1"
+                    />
+                </div>
+                <div className="bar">
+                    <p className="element">나트륨&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                    <ProgressBar
+                        className="progress"
+                        completed={progressData.totalSaltPerDay}
+                        maxCompleted={progressData.recommendedSaltPerDay}
+                        customLabel={`${progressData.totalSaltPerDay}`}
+                        labelClassName="label"
+                        bgColor="#7BDEF2"
+                        barContainerClassName="container2"
+                    />
+                </div>
+                <div className="bar">
+                    <p className="element">단백질&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                    <ProgressBar
+                        className="progress"
+                        completed={progressData.totalProteinPerDay}
+                        maxCompleted={progressData.recommendedProteinPerDay}
+                        customLabel={`${progressData.totalProteinPerDay}`}
+                        labelClassName="label"
+                        bgColor="#E7D6E0"
+                        barContainerClassName="container3"
+                    />
+                </div>
+                <div className="bar">
+                    <p className="element">
+                        지방&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </p>
+                    <ProgressBar
+                        className="progress"
+                        completed={progressData.totalFatPerDay}
+                        maxCompleted={progressData.recommendedFatPerDay}
+                        customLabel={`${progressData.totalFatPerDay}`}
+                        labelClassName="label"
+                        bgColor="#F2B5D4"
+                        barContainerClassName="container4"
+                    />
+                </div>
+            </div>
+            <div className="classify_graph">
+                <p className="description">
+                    당신의 일일 적정 섭취량은 칼로리{" "}
+                    {progressData.recommendedCaloriePerDay}kacl, 나트륨{" "}
+                    {progressData.recommendedSaltPerDay}g, 단백질{" "}
+                    {progressData.recommendedProteinPerDay}g, 지방{" "}
+                    {progressData.recommendedFatPerDay}g 입니다.
+                </p>
+                <p className="description">{analysisText.calorie}</p>
+                <p className="description">{analysisText.salt}</p>
+                <p className="description">{analysisText.protein}</p>
+                <p className="description">{analysisText.fat}</p>
+            </div>
+            <div className="button_group">
+                <button className="home_btn" onClick={moveHomePage}>
+                    홈으로
+                </button>
+            </div>
         </div>
-        <div className="bar">
-          <p className="element">나트륨&nbsp;&nbsp;&nbsp;&nbsp;</p>
-          <ProgressBar
-            className="progress"
-            completed={progressData.totalSaltPerDay}
-            maxCompleted={progressData.recommendedSaltPerDay}
-            customLabel={`${progressData.totalSaltPerDay}`}
-            labelClassName="label"
-            bgColor="#7BDEF2"
-            barContainerClassName="container2"
-          />
-        </div>
-        <div className="bar">
-          <p className="element">단백질&nbsp;&nbsp;&nbsp;&nbsp;</p>
-          <ProgressBar
-            className="progress"
-            completed={progressData.totalProteinPerDay}
-            maxCompleted={progressData.recommendedProteinPerDay}
-            customLabel={`${progressData.totalProteinPerDay}`}
-            labelClassName="label"
-            bgColor="#E7D6E0"
-            barContainerClassName="container3"
-          />
-        </div>
-        <div className="bar">
-          <p className="element">지방&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-          <ProgressBar
-            className="progress"
-            completed={progressData.totalFatPerDay}
-            maxCompleted={progressData.recommendedFatPerDay}
-            customLabel={`${progressData.totalFatPerDay}`}
-            labelClassName="label"
-            bgColor="#F2B5D4"
-            barContainerClassName="container4"
-          />
-        </div>
-      </div>
-      <div className="classify_graph">
-        <p className="description">
-          당신의 일일 적정 섭취량은 칼로리 2100kacl, 탄수화물 1300kacl, 단백질 50g, 지방 70g입니다.
-          현재 당신의 섭취량은 칼로리, 탄수화물, 단백질, 지방 모두 일일 적정량에 미달 상태입니다.
-          일일 적정량을 더 섭취하여 권장량을 맞춰주세요.
-        </p>
-      </div>
-      <div className="button_group">
-        <button className="home_btn" onClick={moveHomePage}>
-          홈으로
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DailyCheck;
